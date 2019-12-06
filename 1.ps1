72e0a51256850fa62fc00faaf728b3b009f4506d
Joe Bush <jpbush.3rd@gmail.com>
2019-12-06 01:52:31 -0800

    clean up day 1

--- 1.ps1
+++ 1.ps1
@@ -1,33 +1,50 @@
-# to run: 
-# (Get-Content .\day1in.txt | ForEach-Object {Resolve-FuelBasic -Mass $_} | Measure-Object -Sum).Sum
-function Resolve-FuelBasic()
+
+function Resolve-FuelBasic
 {
     param(
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        [int]
-        $Mass
+        [int] $Mass
     )
 
     Return [math]::floor($Mass / 3) - 2
 }
 
-# to run: 
-# (Get-Content .\day1in.txt | ForEach-Object {Resolve-FuelBasic -Mass $_} | Measure-Object -Sum).Sum
-function Resolve-FuelRecurse()
+function Resolve-FuelRecurse
 {
     param(
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
-        [int]
-        $Mass
+        [int] $Mass
     )
 
     $fuelMass = [math]::floor($Mass / 3) - 2
-    if($fuelMass -le 0)
-    {
+    if($fuelMass -le 0) {
         return 0
     }
-    else
-    {
+    else {
         return $fuelMass + (Resolve-FuelRecurse -Mass $fuelMass)
     }
+}
+
+function Run-Part1 {
+    [CmdletBinding()]
+    param(
+        [string] $InFilename
+    )
+    $total = 0
+    foreach ($mass in Get-Content $InFilename) {
+        $total += Resolve-FuelBasic -Mass $mass
+    }
+
+    Write-Host "Total fuel needed: $total"
+}
+
+function Run-Part2 {
+    [CmdletBinding()]
+    param(
+        [string] $InFilename
+    )
+    $total = 0
+    foreach ($mass in Get-Content $InFilename) {
+        $total += Resolve-FuelRecurse -Mass $mass
+    }
+    
+    Write-Host "Total fuel needed: $total"
 }
