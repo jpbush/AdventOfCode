04a156cf00f4dd53ad1112506180fb446139d1d4
Joe Bush <jpbush.3rd@gmail.com>
2019-12-06 03:39:21 -0800

    Add empty files

---
+++ 2019/7/7.ps1
@@ -0,0 +1,16 @@
+
+function Run-Part1
+{
+    [CmdletBinding()]
+    param(
+        [string] $InFilename
+    )
+}
+
+function Run-Part2
+{
+    [CmdletBinding()]
+    param(
+        [string] $InFilename
+    )
+}
