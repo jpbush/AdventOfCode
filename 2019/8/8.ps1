1934ce91e464bf0b1da1fada828f08c2605aa106
Joe Bush <jpbush.3rd@gmail.com>
2019-12-07 23:18:26 -0800

    p2

--- 2019/8/8.ps1
+++ 2019/8/8.ps1
@@ -25,7 +25,7 @@ function Run-Part1
         [int] $searchNum
     )
     
-    "searchNum: $searchNum"
+    Write-Verbose "searchNum: $searchNum"
 
     $imgStr = (Get-Content -Path $InFilename)
     $layerSize = $Width * $Height
@@ -70,10 +70,60 @@ function Run-Part1
     Write-Host "Answer: $answer"
 }
 
+# 0 is black, 1 is white, and 2 is transparent.
 function Run-Part2
 {
     [CmdletBinding()]
     param(
-        [string] $InFilename
+        [string] $InFilename,
+        [int] $Width,
+        [int] $Height,
+        [int] $searchNum
     )
+    "searchNum: $searchNum"
+
+    $outFilename = "out.txt"
+    Remove-Item $outFilename
+
+    $imgStr = (Get-Content -Path $InFilename)
+    $layerSize = $Width * $Height
+    Write-Verbose "layerSize: $layerSize"
+    $imgLayers = @()
+    for ($i = 0; $i -lt $imgStr.Length; $i += $layerSize) {
+        $imgLayers += ,@($imgStr[$i..($i+$layerSize-1)])
+    }
+    $layerCount = $imgLayers.Length
+    Write-Verbose "LayerCount: $layerCount"
+    
+    for($j = 0; $j -lt $layerCount; $j++) {
+        $layer = $imgLayers[$j]
+        Write-Verbose "Layer $layer"
+    }
+
+    $minCount = $null
+    $minLayer = $null
+    $minLayerIndex = $null
+    $output = ,-1 * $layerSize
+    for ($i = 0; $i -lt $layerSize; $i++) {
+        $colorFound = 2
+        for($j = 0; $j -lt $layerCount; $j++) {
+            $layer = $imgLayers[$j]
+            $color = $layer[$i]
+            Write-Verbose "Layer $j processed for pixel $i, color: $color"
+            if ($color -ne '2') {
+                $colorFound = $color
+                break
+            }
+        }
+        $output[$i] = $colorFound
+    }
+
+    for($i = 0; $i -lt $layerSize; $i+=$Width) {
+        $row = $output[$i..($i+$Width-1)]
+
+        $result = ($row -join ",")
+        Write-Host $result
+        $result | Out-File -FilePath $outFilename -Append
+    }
+    # "sep=," | Out-File -FilePath $outFilename -Append
 }
