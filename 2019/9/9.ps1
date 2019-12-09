1fd3757fe6c8561059f9c6c248efb6a886a406c9
Joe Bush <jpbush.3rd@gmail.com>
2019-12-09 11:16:29 -0800

    Make it easier to run in debugger

--- 2019/9/9.ps1
+++ 2019/9/9.ps1
@@ -481,26 +481,3 @@ function Run-OpCodes
     $state.isHalted = $true
     return $state
 }
-
-function Run-Part1
-{
-    [CmdletBinding()]
-    param(
-        [string] $InFilename,
-        [int[]] $In
-    )
-
-    $content = (Get-Content $InFilename)
-    $program = [ProgramState]::New($content.split(','), 0, 0, $In, @(), 0)
-    $program = Run-OpCodes -state $program
-    Write-Verbose "program: [$($content.split(',') -join ',')]"
-    Write-Host "output:  [$($program.outBuff -join ',')]"
-}
-
-function Run-Part2
-{
-    [CmdletBinding()]
-    param(
-        [string] $InFilename
-    )
-}
